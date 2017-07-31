5e5b6dc52f7c117c2f8594db249cdcc7db7efa75
SillyFreak <sillyfreak.ck@gmail.com>
2017-07-31 13:04:19 +0200

    add ReadDigital

---
+++ src/components/ReadDigital.ts
@@ -0,0 +1,55 @@
+import "babel-polyfill";
+import {HedgehogClient} from 'hedgehog-client';
+import {connectionStore} from '../lib/ConnectionStore';
+
+let noflo: any = require('noflo');
+
+export function getComponent() {
+    let c = new noflo.Component();
+    c.description = 'reads a digital sensor';
+    c.icon = 'area-chart';
+    c.inPorts.add('in', {
+        datatype: 'bang',
+        description: 'signal to trigger a sensor request',
+    });
+    c.inPorts.add('port', {
+        datatype: 'number',
+        description: 'the sensor port',
+        control: true,
+        triggering: false,
+    });
+    c.inPorts.add('endpoint', {
+        datatype: 'string',
+        description: 'the ZMQ endpoint to talk to',
+        control: true,
+        triggering: false,
+    });
+    c.outPorts.add('out', {
+        datatype: 'boolean',
+        description: 'the sensor value'
+    });
+
+    return c.process((input, output) => {
+        if(!input.hasData('in')) return;
+        input.get('in');
+
+        if(!(input.hasData('port') && input.hasData('endpoint'))) {
+            output.done();
+            return;
+        }
+
+        let port: number = input.getData('port');
+        let endpoint: string = input.getData('endpoint');
+        if(!endpoint) {
+            output.done();
+            return;
+        }
+
+        let hedgehog: HedgehogClient = connectionStore.getConnection(endpoint);
+        hedgehog.getDigital(port).then((value: boolean) => {
+            output.sendDone({
+                out: value
+            });
+        });
+    });
+}
